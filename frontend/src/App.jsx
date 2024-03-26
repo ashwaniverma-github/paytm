066a938c00d1c1a060126970905282075e0a9ce8
ashwaniverma-github <ap8606574@gmail.com>
2024-03-26 18:37:49 +0530

    added sign-up page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {BrowserRouter,Route,Routes} from "react-router-dom";
+import { Signup } from "./pages/Signup";
+// import { Signin } from "./pages/Signin";
+// import { Dashboard } from "./pages/Dashboard";
+// import { SendMoney } from "./pages/SendMoney";
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
 
-  return (
+  return(
     <>
-      <div className='flex justify-between'>
-        <div>hello</div>
-        <div>hello</div>
-      </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/signup" element={<Signup/>}/>
+      {/* <Route path="/signin" element={<Signin/>}/>
+      <Route path="/SendMoney" element={<SendMoney/>}/>
+      <Route path="Dashboard" element={<Dashboard/>}/> */}
+    </Routes>
+    </BrowserRouter>
     </>
+
   )
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,25 @@
+import {Heading} from "../components/Heading";
+import {SubHeading} from "../components/SubHeading"
+import { InputBox } from "../components/InputBox";
+import { Button } from "../components/Button";
+import { BottomWarning } from "../components/BottomWarning";
+
+export function Signup(){
+    return <div className="bg-slate-500 h-screen flex justify-center" >
+        <div className="flex flex-col justify-center">
+            <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4" >
+                <Heading label="Sign up"/>
+                <SubHeading label="Enter your information to create an acoount" />
+                <InputBox label="First Name" placeholder="John" />
+                <InputBox label="last Name" placeholder="doe" />
+                <InputBox label="Username" placeholder="john@gmail.com" />
+                <InputBox label="Password" placeholder="Enter password" />
+                <div className="pt-4" >
+                    <Button label="Sign up" />
+                </div>
+                <BottomWarning label={"Already have an acoount"} buttonText={"Sign in"} to={"/signin"}/>
+                
+            </div>
+        </div>      
+    </div>
+}
